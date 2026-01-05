197c2cd7e85c085f162f9162861eda51e4d7d2c8
AnaeleJoshua <anaelejoshua@gmail.com>
2026-01-05 11:53:23 +0000

    refactor(book):added a repository layer for abstracting  db interaction

--- src/features/book/repository/book.repository.ts
+++ src/features/book/repository/book.repository.ts
@@ -1,31 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../prisma.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 
 @Injectable()
 export class BookRepository {
-	constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-	findAll() {
-		return this.prisma.book.findMany();
-	}
+  findAll() {
+    return this.prisma.book.findMany();
+  }
 
-	create(data: { title: string; description: string }) {
-		return this.prisma.book.create({ data });
-	}
+  create(data: { title: string; description: string }) {
+    return this.prisma.book.create({ data });
+  }
 
-	update(
-		id: number,
-		data: { title?: string; description?: string },
-	) {
-		return this.prisma.book.update({
-			where: { id },
-			data,
-		});
-	}
+  update(id: number, data: { title?: string; description?: string }) {
+    return this.prisma.book.update({
+      where: { id },
+      data,
+    });
+  }
 
-	delete(id: number) {
-		return this.prisma.book.delete({
-			where: { id },
-		});
-	}
+  delete(id: number) {
+    return this.prisma.book.delete({
+      where: { id },
+    });
+  }
 }
