04cf757ad4fb5936a480c9bdec15c4eb9550de94
AnaeleJoshua <anaelejoshua@gmail.com>
2026-01-06 11:44:05 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createServer } from './src/main';
+import { createServer } from './main.serveless.';
 
 export default async function handler(req, res) {
   const server = await createServer();
