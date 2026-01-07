b8d84ba9695e7069d299b9f4d8f98060d8ef63aa
AnaeleJoshua <anaelejoshua@gmail.com>
2026-01-07 17:18:41 +0000

    Refactor(auth0.guard):refactor the whole module and used the jose library, to resolve the jwt verification errors

--- src/Auth-config/auth0.guard.ts
+++ src/Auth-config/auth0.guard.ts
@@ -6,13 +6,12 @@ import {
 } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { ConfigService } from '@nestjs/config';
-import { createRemoteJWKSet, jwtVerify } from 'jose';
 
 @Injectable()
 export class Auth0Guard implements CanActivate {
-  private jwks: ReturnType<typeof createRemoteJWKSet>;
   private issuer: string;
   private audience: string;
+  private jwks: any;
 
   constructor(private readonly configService: ConfigService) {
     this.issuer = this.configService.get<string>('AUTH0_ISSUER_URL')!;
@@ -21,15 +20,22 @@ export class Auth0Guard implements CanActivate {
     if (!this.issuer || !this.audience) {
       throw new Error('Auth0 config missing');
     }
-    
+  }
+
+  private async initJwks() {
+    if (this.jwks) return;
+
+    const { createRemoteJWKSet } = await import('jose');
     this.jwks = createRemoteJWKSet(
-      new URL(`${this.issuer}.well-known/jwks.json`),
+      new URL(`${this.issuer}.well-known/jwks.json`)
     );
-    console.log(`jwks: ${this.jwks}`)
-    
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    await this.initJwks();
+
+    const { jwtVerify } = await import('jose');
+
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
 
